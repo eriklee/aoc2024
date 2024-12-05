143134d1d54726c3284678fba739e35caf36f683
Erik Lee <eriklee113@gmail.com>
2024-12-05 03:42:48 +0000

    Day 1
    some very straightforward haskell I have not managed to make work nicely with nix yet, but maybe someday...

---
+++ README.md
@@ -0,0 +1,10 @@
+AOC 2024
+========
+
+I will probably have a go at doing it mostlly in haskell this year and maybe a bit of zig.
+
+I still haven't managed to get any sort of haskell workflow working nicely in nix, but I'll hopefully manage to finally get that figured out by the weekend so I don't have to write everything from scratch myself.
+
+in the meantime:
+`$ nix-shell -p ghc`
+`$ runhaskell src/day1.hs`
---
+++ src/day1.hs
@@ -0,0 +1,31 @@
+import Data.List (sort)
+import Data.Map as M
+
+main :: IO ()
+main = do
+  inp <- lines <$> readFile "inp/day1.txt"
+  let (inpL,inpR) = both sort . unzip $ parseLine <$>  inp
+  putStr "Day1: Part1: "
+  print $ part1 inpL inpR
+  -- 3574690
+  putStr "Day1: Part2: "
+  print $ part2 inpL inpR
+
+parseLine :: String -> (Int, Int)
+parseLine x = let [l,r] = words x
+  in (read l, read r)
+
+part1 :: [Int] -> [Int] -> Int
+part1 l r = sum $ abs <$> zipWith (-) l r
+
+buildMap :: [Int] -> M.Map Int Int
+buildMap = M.fromListWith (+) . fmap (,1)
+
+calcSimilarityScore :: M.Map Int Int -> Int -> Int
+calcSimilarityScore m x = maybe 0 (*x) (M.lookup x m)
+
+part2 :: [Int] -> [Int] -> Int
+part2 l r = sum $ fmap (calcSimilarityScore $ buildMap r) l
+
+both :: (a -> b) -> (a,a) -> (b,b)
+both f (x,y) = (f x, f y)
